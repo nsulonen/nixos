bb47a2af048d06934cb4f2aa6faef5f4df697539
nsulonen <niko.sulonen@proton.me>
2025-06-18 15:17:02 +0300

    initial flake config

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+    description = "A simple Nix flake for system configuration";
+    
+    inputs = {
+        # Nixpkgs
+        nixpkgs = "github:NixOS/nixpkgs/nixos-25.05";
+
+        # Home Manager
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {self, nixpkgs, home-manager, ...} @ inputs: let
+        inherit (self) outputs;
+    in {
+        # Define the NixOS system configuration
+        nixosConfigurations = {
+            nixos = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                specialArgs = { inherit inputs outputs; };
+                modules = [ ./nixos/configuration.nix ];
+            };
+        };
+
+        # Home Manager configurations
+        homeConfigurations = {
+            "niko@nixos" = home-manager.lib.homeManagerConfiguration {
+                pkgs = nixpkgs.legacyPackages.x86_64-linux;
+                extraSpecialArgs = { inherit inputs outputs; };
+                modules = [ ./home-manager/home.nix ];
+            };
+        };
+    };
+}
