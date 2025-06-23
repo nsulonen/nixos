bb22ce0cdcca23d027f445901557cce042da5e4d
nsulonen <niko.sulonen@proton.me>
2025-06-23 15:34:44 +0300

    fix typo

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
                     {
                         home-manager.useGlobalPkgs = true;
                         home-manager.useUserPackages = true;
-                        home-manager.users.niko = import ./home.nix
+                        home-manager.users.niko = import ./home.nix;
                     }
                 ];
             };
