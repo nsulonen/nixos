ed25e975ebf3aebf176c058343ad5e59610a196b
nsulonen <niko.sulonen@proton.me>
2025-06-23 19:55:49 +0300

    move packages and programs to home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,8 +5,7 @@
   pkgs,
   ...
 }: {
-
-
+  
   home = {
     username = "niko";
     homeDirectory = "/home/niko";
@@ -14,6 +13,33 @@
 
   # Enable Home Manager features
   programs.home-manager.enable = true;
+
+  # Home Packages
+  home.packages = with pkgs; [
+    gh
+    vscode
+    distrobox
+  ];
+
+  # Programs
+  programs = {
+    git = {
+      enable = true;
+      userName = "nsulonen";
+      userEmail = "niko.sulonen@proton.me";
+      init.defaultBranch = "main";
+    };
+    firefox = {
+      enable = true;
+    };
+    steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    gamemode = {
+      enable = true;
+    };
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -78,20 +78,8 @@
   environment.systemPackages = with pkgs; [
     git
     vim
-    gh
-    vscode
     wget
-    distrobox
   ];
-
-  programs.firefox.enable = true;
-
-  programs.steam = {
-    enable = true;
-    gamescopeSession.enable = true;
-  };
-
-  programs.gamemode.enable = true;
 
 
   #users
