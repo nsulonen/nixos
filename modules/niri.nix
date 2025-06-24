328649a196d08edba2b8eb3a75ce4ed5d870cac7
nsulonen <niko.sulonen@proton.me>
2025-06-24 19:44:09 +0300

    create module for niri

---
+++ modules/niri.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+{
+    programs.niri.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      alacritty
+      swaylock
+      waybar
+      mako
+      fuzzel
+    ];
+}
