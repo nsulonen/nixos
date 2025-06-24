2318eb375d5a609cb329225f86b2b182a5a5f7b7
nsulonen <niko.sulonen@proton.me>
2025-06-24 18:23:32 +0300

    minor changes

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
     description = "A simple Nix flake for system configuration";
     
     inputs = {
+
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
         nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
@@ -10,9 +11,11 @@
             url = "github:nix-community/home-manager/release-25.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        
     };
 
     outputs = { nixpkgs, nix-flatpak, home-manager, ... }: {
+
         nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
