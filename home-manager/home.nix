1a6446e71577b07311bf9c7a84646d5b8637a860
nsulonen <niko.sulonen@proton.me>
2025-06-24 20:04:51 +0300

    add default niri config to home manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,6 +14,12 @@
   # Enable Home Manager features
   programs.home-manager.enable = true;
 
+  # Manage configurations with Home Manager
+  home.file = {
+
+    # Configuration file for Niri
+    ".config/niri/config.kdl".source = ./dots/config.kdl;
+  };
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
