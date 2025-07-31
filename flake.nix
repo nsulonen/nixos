2091609da560fe20ef68b51204871e5e8e59affe
nsulonen <niko.sulonen@proton.me>
2025-07-31 16:27:27 +0300

    move home manager to nixos module

--- flake.nix
+++ flake.nix
@@ -22,12 +22,19 @@
             modules = [
                 ./nixos/system.nix
                 nix-flatpak.nixosModules.nix-flatpak
+                home-manager.nixosModules.home-manager {
+                    home-manager.users.niko = {
+                        imports = [
+                            ./home-manager/home.nix
+                        ];
+                    };
+                }
             ];
         };
 
-        homeConfigurations.niko = home-manager.lib.homeManagerConfiguration {
-            pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            modules = [ ./home-manager/home.nix ];
-        };
+        #homeConfigurations.niko = home-manager.lib.homeManagerConfiguration {
+        #    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        #    modules = [ ./home-manager/home.nix ];
+        #};
     };          
 }
