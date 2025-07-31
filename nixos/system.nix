d87bed334340578e8c23e4140cf5c5c74fb7c37e
nsulonen <niko.sulonen@proton.me>
2025-07-31 16:52:02 +0300

    add theme settings

--- nixos/system.nix
+++ nixos/system.nix
@@ -135,5 +135,28 @@
     trusted-users = ["root" "niko"];
     experimental-features = [ "nix-command" "flakes" ];
   };
+
+  #theme
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Breeze-Dark";
+      package = pkgs.libsForQt5.breeze-gtk;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.catppuccin-papirus-folders.override {
+        flavor = "mocha";
+        accent = "lavender";
+      };
+    };
+    cursorTheme = {
+      name = "Catppuccin-Mocha-Light-Cursors";
+      package = pkgs.catppuccin-cursors.mochaLight;
+    };
+    gtk3 = {
+      extraConfig.gtk-application-prefer-dark-theme = true;
+    };
+  };
 }
 
