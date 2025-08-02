dab200d67c270bd96f2aca502459db3c32ddde42
nsulonen <niko.sulonen@proton.me>
2025-08-02 23:24:47 +0300

    tokyo night theme

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -1,7 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  stylix.enable = true;  
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-  # stylix.image = ../../home-manager/walls/wall6.jpg;
+  stylix = {
+    enable = true;  
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-city-dark.yaml";
+    # image = ../../home-manager/walls/wall6.jpg;
+  };
 }
