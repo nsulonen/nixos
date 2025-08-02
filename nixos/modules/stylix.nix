df3c66267bcf4869c2734a0a90eae5a26cf9846f
nsulonen <niko.sulonen@proton.me>
2025-08-02 23:41:32 +0300

    change wallpaper

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;  
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    image = ../../home-manager/walls/wall3.jpg;
+    image = ../../home-manager/walls/wall.png;
     polarity = "dark";
   };
 }
