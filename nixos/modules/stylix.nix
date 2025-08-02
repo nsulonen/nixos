9096347c7691f0a26320868196d3c1f2caf9afcc
nsulonen <niko.sulonen@proton.me>
2025-08-02 23:38:48 +0300

    prefer dark polarity

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -5,5 +5,6 @@
     enable = true;  
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     image = ../../home-manager/walls/wall3.jpg;
+    polarity = "dark";
   };
 }
