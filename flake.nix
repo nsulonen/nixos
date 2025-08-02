1914fbe65897d70165d260693a1509c6ee6a3c88
nsulonen <niko.sulonen@proton.me>
2025-08-02 17:05:45 +0300

    bring in niri-flake and stylix

--- flake.nix
+++ flake.nix
@@ -5,31 +5,34 @@
 
         nixpkgs.url = "nixpkgs/nixos-25.05";
 
-        nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+        niri.url = "github:sodiboo/niri-flake";
+
+        stylix.url = "github:nix-community/stylix/release-25.05";
 
         home-manager = {
             url = "github:nix-community/home-manager/release-25.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
         
+        # nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     };
 
-    outputs = { nixpkgs, nix-flatpak, home-manager, ... }@inputs: {
+    outputs = { nixpkgs, stylix, niri, home-manager, ... }@inputs: {
 
         nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             specialArgs = { inherit inputs; };
             modules = [
                 ./nixos/desktop
-                nix-flatpak.nixosModules.nix-flatpak
-
+                # nix-flatpak.nixosModules.nix-flatpak
+                stylix.nixosModules.stylix
+                niri.nixosModules.niri
+                
                 home-manager.nixosModules.home-manager {
-                    
                     home-manager = {
                         useGlobalPkgs = false;
                         useUserPackages = true;
                         backupFileExtension = "backup";
-
                         users.niko = {
                             imports = [ ./home-manager/home.nix ];
                         };
