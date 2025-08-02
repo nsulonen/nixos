701ad9bf1a5e67c27b4fb257b422e2b79760371a
nsulonen <niko.sulonen@proton.me>
2025-08-02 20:33:40 +0300

    stylix theming

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -3,5 +3,5 @@
 {
   stylix.enable = true;  
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.image = ../../home-manager/walls/nixos.jpg;
+  stylix.image = ../../home-manager/walls/wall6.jpg;
 }
