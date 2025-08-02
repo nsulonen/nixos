a2828f18bff29dd94217eaa4597d421baeaefc29
nsulonen <niko.sulonen@proton.me>
2025-08-02 20:14:36 +0300

    let stylix generate theme based on wallpaper

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -2,5 +2,6 @@
 
 {
   stylix.enable = true;  
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  stylix.image = ../../home-manager/walls/wall9.jpg;
 }
