5648129e49200725014fb9c683a3cd38f9724a92
nsulonen <niko.sulonen@proton.me>
2025-08-03 21:17:20 +0300

    change wallpaper and theme

--- home-manager/modules/configs/niri/settings.nix
+++ home-manager/modules/configs/niri/settings.nix
@@ -82,7 +82,7 @@
 		spawn-at-startup = [
 			{ command = ["waybar"]; }
 			{ command = ["xwayland-satellite"]; }
-			{ command = ["swaybg" "-o" "*" "-i" "/home/niko/System/home-manager/walls/nebula.png" "-m" "fill"]; }
+			{ command = ["swaybg" "-o" "*" "-i" "/home/niko/System/home-manager/walls/gradient3.png" "-m" "fill"]; }
 		];
 
 		environment = {
