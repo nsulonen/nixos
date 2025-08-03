85bf31ab259a889664b291cebf263752bf732504
nsulonen <niko.sulonen@proton.me>
2025-08-03 14:02:49 +0300

    new wallpaper and theme

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;  
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/onedark-dark.yaml";
     # image = ../../home-manager/walls/nixos.jpg;
     # polarity = "dark";
   };
