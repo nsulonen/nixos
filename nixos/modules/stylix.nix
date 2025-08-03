77c24033c6795f38ba8266981b5d42901d688e83
nsulonen <niko.sulonen@proton.me>
2025-08-03 20:45:19 +0300

    change wallpaper and theme

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;  
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     # image = ../../home-manager/walls/nixos.jpg;
     # polarity = "dark";
   };
